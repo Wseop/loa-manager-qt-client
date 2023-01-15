3fc4f32d467f834269d8bde64698539283349f1d
wooseop <crazyz2000@naver.com>
2023-01-15 17:30:55 +0900

    CLN:Remove test code

--- main.cpp
+++ main.cpp
@@ -2,15 +2,10 @@
 
 #include <QApplication>
 
-#include "game_data/character/character_manager.h"
-
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     LoaManager w;
     w.show();
-
-    CharacterManager::getInstance()->getCharacter("쿠키바닐라쉐이크");
-
     return a.exec();
 }

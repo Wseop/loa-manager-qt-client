4216c9afcb9d72302c4ecd11df2b09440074ce51
wooseop <crazyz2000@naver.com>
2023-01-16 13:51:34 +0900

    FEAT:Implement function - createLabel

--- ui/widget_manager.cpp
+++ ui/widget_manager.cpp
@@ -1,11 +1,22 @@
 #include "widget_manager.h"
 #include "ui/font_manager.h"
 #include <QPushButton>
+#include <QLabel>
+
+FontManager* WidgetManager::m_pFontManager = FontManager::getInstance();
 
 QPushButton* WidgetManager::createPushButton(QString text, int width, int height, int fontSize, QWidget* pParent)
 {
     QPushButton* pButton = new QPushButton(text, pParent);
     pButton->setFixedSize(width, height);
-    pButton->setFont(FontManager::getInstance()->getFont(FontFamily::NanumSquareNeoBold, fontSize));
+    pButton->setFont(m_pFontManager->getFont(FontFamily::NanumSquareNeoBold, fontSize));
     return pButton;
+}
+
+QLabel* WidgetManager::createLabel(QString text, int width, int height, int fontSize, QWidget *pParent)
+{
+    QLabel* pLabel = new QLabel(text, pParent);
+    pLabel->setFixedSize(width, height);
+    pLabel->setFont(m_pFontManager->getFont(FontFamily::NanumSquareNeoBold, fontSize));
+    return pLabel;
 }
--- ui/widget_manager.h
+++ ui/widget_manager.h
@@ -9,6 +9,10 @@ class WidgetManager : public QObject
 
 public:
     static class QPushButton* createPushButton(QString text, int width, int height, int fontSize = 10, QWidget* pParent = nullptr);
+    static class QLabel* createLabel(QString text, int width, int height, int fontSize = 10, QWidget* pParent = nullptr);
+
+private:
+    static class FontManager* m_pFontManager;
 };
 
 #endif // WIDGETMANAGER_H

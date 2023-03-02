825978dca998f9d02a8d4f36915dc2f1f2b5180f
wooseop <crazyz2000@naver.com>
2023-03-02 14:46:16 +0900

    CLN:Refactor Engrave codes

---
+++ game/engrave/engrave.h
@@ -0,0 +1,29 @@
+#ifndef ENGRAVE_H
+#define ENGRAVE_H
+
+#include <QString>
+#include <QHash>
+
+class Engrave
+{
+public:
+    Engrave();
+
+    const QStringList getEngraves() const;
+    const QStringList getPenalties() const;
+    int getEngraveValue(const QString& engrave) const;
+    int getPenaltyValue(const QString& penalty) const;
+    int getEngraveLevel(const QString& engrave) const;
+    int getPenaltyLevel(const QString& penalty) const;
+
+    void addEngrave(const QString& engrave, int value);
+    void addPenalty(const QString& penalty, int value);
+
+private:
+    const int VALUE_PER_LEVEL = 5;
+
+    QHash<QString, int> m_engraveValues;
+    QHash<QString, int> m_penaltyValues;
+};
+
+#endif // ENGRAVE_H
